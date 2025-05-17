d3b13a51bdee7503da7761c19d503fc0ff19369a
Malyavantham Lahari <mlahari252@gmail.com>
2025-05-18 00:42:49 +0530

    Update ALLOWED_HOSTS to fix DisallowedHost error

--- Codeeditor/settings.py
+++ Codeeditor/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+CSRF_TRUSTED_ORIGINS= ["codeeditor-production-2c20.up.railway.app"]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +26,9 @@ SECRET_KEY = 'django-insecure-=so#xzk&l)$k_huh2jwk-ihna3fs2mjk#h0obe%+uec^m&_igl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codeeditor-production-2c20.up.railway.app', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS= ["codeeditor-production-2c20.up.railway.app"]
+ALLOWED_HOSTS = [""]
+
+
 
 
 # Application definition
