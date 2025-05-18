fc5f801bd9d94e3d90706ae488dfe8b0ddd76502
Malyavantham Lahari <mlahari252@gmail.com>
2025-05-18 14:30:59 +0530

    'update'

--- Codeeditor/settings.py
+++ Codeeditor/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-CSRF_TRUSTED_ORIGINS= ["codeeditor-production-2c20.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ['https://codeeditor-production-2c20.up.railway.app']
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
